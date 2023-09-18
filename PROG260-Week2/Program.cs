853cb6f58104f5806ea1d0cda19fdc7ac66b5cfe
agart001 <agart.art@gmail.com>
2023-09-18 15:51:07 -0500

    Add project files.

---
+++ PROG260-Week2/Program.cs
@@ -0,0 +1,31 @@
+﻿using static PROG260_Week2.Utility;
+
+namespace PROG260_Week2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string Base = BaseDir();
+
+            Dictionary<string, string> dict = ReadAll("txt", ".txt");
+
+            foreach (KeyValuePair<string, string> kvp in dict)
+            {
+                Console.WriteLine($"File: {kvp.Key}");
+                Dictionary<char[], int[]> charcounts = CharCount(kvp);
+                char[] chars = charcounts.First().Key.ToArray();
+                int[] counts = charcounts.First().Value.ToArray();
+
+                int index = 0;
+                foreach(char c in chars)
+                {
+                    Console.WriteLine($"{c} : {counts[index]}");
+                    index++;
+                }
+            };
+        }
+    }
+}
---
+++ PROG260-Week2/Utility.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Collections.Concurrent;
+using System.Collections;
+
+namespace PROG260_Week2
+{
+    public static class Utility
+    {
+        public static DirectoryInfo UseableBaseDir = new DirectoryInfo(BaseDir());
+
+        public static string BaseDir()
+        {
+            string str = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+
+            return str;
+        }
+
+        public static int GetLetterCount(string input, char lookup)
+        {
+            char[] chars = input.ToCharArray();
+            return chars.Where(c => c == lookup).Count();
+        }
+
+        public static int[] CountsArray(char[] chars, string lookup)
+        {
+            int[] result = new int[chars.Length];
+            int index = 0;
+            foreach(char c in chars)
+            { 
+                result[index] = GetLetterCount(lookup, c);
+                index++;
+            }
+            return result;
+        }
+
+       
+        public static Dictionary<string, string> ReadAll(string dir, string type)
+        {
+            ConcurrentDictionary<string, string> FileLines = new ConcurrentDictionary<string, string>();
+
+            string path = Path.Combine(UseableBaseDir.FullName, dir);
+
+            List<string> paths = Directory.EnumerateFiles(path, $"*{type}").ToList();
+
+            Parallel.ForEach(paths, current => {
+                string[] strings = File.ReadAllLines(current);
+                string filestring = strings.Aggregate((sum, next) => sum += next);
+
+                FileLines.TryAdd(Path.GetFileName(current), filestring);
+            });
+
+            return FileLines.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+
+        
+        public static Dictionary<char[], int[]> CharCount(KeyValuePair<string, string> keyValuePair)
+        {
+            Dictionary<char[], int[]> CharCounts = new Dictionary<char[], int[]>();
+
+            char[] distinctChars = keyValuePair.Value.Distinct().ToArray();
+            int[] charCounts = CountsArray(distinctChars, keyValuePair.Value);
+
+            CharCounts.Add(distinctChars, charCounts);
+            
+            return CharCounts;
+        } 
+    }
+}
